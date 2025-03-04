d87d45bf54ae2c1bdbc22dc787acd3a4fae8f381
soye00 <ssssssy0@naver.com>
2025-03-04 11:23:47 +0900

    [level 0] Title: 모음 제거, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.js
@@ -0,0 +1,14 @@
+function solution(my_string) {
+    var answer = '';
+    for (let i = 0; i < my_string.length; i++) {
+        if(my_string[i] === 'a') {}
+        else if(my_string[i] === 'e') {}
+        else if(my_string[i] === 'i') {}
+        else if(my_string[i] === 'o') {}
+        else if(my_string[i] === 'u') {}
+        else {
+            answer = answer + my_string[i];
+        }
+    }
+    return answer;
+}
