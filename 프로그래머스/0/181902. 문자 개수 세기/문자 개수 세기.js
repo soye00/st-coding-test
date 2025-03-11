21415acaa5f129e2f4d4989fc7116e53e3ed8750
soye00 <ssssssy0@naver.com>
2025-03-11 16:42:18 +0900

    [level 0] Title: 문자 개수 세기, Time: 5.82 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.js
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.js
@@ -1,13 +1,4 @@
 function solution(my_string) {
-    var str = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
-        'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-        'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r'
-        ,'s', 't','u','v','w','x','y','z'];
-    const answer = new Array(str.length).fill(0);
-        for (let i = 0; i < str.length; i++) {
-            for(let j = 0; j < my_string.length; j++) {
-                if(str[i] === my_string[j]) answer[i]++;
-            }
-        }
+    var answer = [];
     return answer;
 }
