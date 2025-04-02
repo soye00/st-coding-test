b2afdab67551c8023e924852d9860b4c6429505a
soye00 <ssssssy0@naver.com>
2025-04-02 13:10:34 +0900

    [level 0] Title: 가위 바위 보, Time: 0.06 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.js
@@ -0,0 +1,8 @@
+function solution(rsp) {
+    var answer = rsp.split('').map((item) => {
+        if(item==2) return 0;
+        if(item==0) return 5;
+        if(item==5) return 2;
+    }).join('');
+    return answer;
+}
